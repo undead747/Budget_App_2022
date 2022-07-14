da3b75b97c90c8e39964571ea927144f80f93925
Minh Tran Cong <minh.trancong@VP1-SE-CL175-01>
2022-07-14 17:34:50 +0900

    temp 20220714

--- src/Components/Form/TaskForm/CurrencyModal.js
+++ src/Components/Form/TaskForm/CurrencyModal.js
@@ -15,35 +15,49 @@ export function useCurrencyModal() {
     const [currencyRates, setCurrencyRates] = useState();
     const [filterCurrencyRates, setFilterCurrencyRates] = useState();
     const { countriesCurrencyInfo, setLoading } = useHomeController();
+    const [selectedRow, setSelectedRow] = useState();
 
     const handleCurrencySearch = (event) => {
       let searchString = event.target.value;
-      if (!isEmptyOrSpaces(searchString)) {
-          setTimeout(() => {
-            let isoList = handleSearchByCountriesInfor(searchString);
-          }, 100)
+
+      if (isEmptyOrSpaces(searchString)) {
+        setFilterCurrencyRates(currencyRates);
+        return
       }
+
+      setTimeout(() => {
+        let isoList = handleSearchByCountriesInfor(searchString);
+        let filters = currencyRates.filter(item => isoList.includes(item.name));
+        setFilterCurrencyRates(filters);
+      }, 100)
     }
 
     const handleSearchByCountriesInfor = (searchString) => {
-      const currencyResults = [];
+      let currencyResults = [];
 
       if (countriesCurrencyInfo) {
+        searchString = searchString.toLowerCase();
         countriesCurrencyInfo.forEach(info => {
-          if (info.iso.includes(searchString) ||
-            info.currency.includes(searchString) ||
-            info.countryName.includes(searchString)) currencyResults.push(info.iso);
+          if (info.iso.toLowerCase().includes(searchString) ||
+            info.currency.toLowerCase().includes(searchString) ||
+            info.countryName.toLowerCase().includes(searchString)) currencyResults.push(info.currency);
         })
       }
 
+      currencyResults = currencyResults.filter((element, index) => currencyResults.indexOf(element) == index);
+
       return currencyResults;
     }
+
+    const handleSelectCurrencyRow = (currency) => setSelectedRow(currency.name);
 
     const handleSubmit = () => {
     };
 
     useEffect(() => {
       if (selectedCurrency) {
+        setSelectedRow(selectedCurrency);
+
         getCurrencyRateByCode(selectedCurrency).then(data => {
           let currencies = [];
 
@@ -74,12 +88,13 @@ export function useCurrencyModal() {
         </Modal.Header>
         <Modal.Body>
           <div>
+            <p>Selected Currency: {selectedRow}</p>
             <input
               type="text"
               className="form-control task-form_currency-search"
               id="task-form_currency-search"
               onChange={handleCurrencySearch}
-              placeholder="currency code"
+              placeholder="name, code, iso"
             />
             <div className="task-form__currency-table-warapper">
               <table className="table task-form__currency-table">
@@ -87,9 +102,10 @@ export function useCurrencyModal() {
                   {filterCurrencyRates &&
                     filterCurrencyRates.map((currency) => (
                       <tr
+                      onClick={() => handleSelectCurrencyRow(currency)}
                         key={currency.name}
                         className={
-                          currency.name === selectedCurrency
+                          currency.name === selectedRow
                             ? "currency--active"
                             : ""
                         }
